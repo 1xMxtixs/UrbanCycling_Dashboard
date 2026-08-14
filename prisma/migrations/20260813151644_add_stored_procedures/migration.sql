d6da762242697e9ce8dd91d67129aa6a1bc1d112
Ignacio Fredes <ignaciofredes780@gmail.com>
2026-08-14 11:02:40 -0400

    feat: añadido SP calcular_total_orden_de_trabajo

--- prisma/migrations/20260813151644_add_stored_procedures/migration.sql
+++ prisma/migrations/20260813151644_add_stored_procedures/migration.sql
@@ -1,1 +1,74 @@
--- This is an empty migration.
+-- =============================================
+-- STORED PROCEDURES DE URBAN CYCLING
+-- =============================================
+
+-- =============================================
+-- Aclaración de atributos
+-- =============================================
+-- DE TABLA "lineas_de_orden_de_trabajo"
+-- precio_unitario: precio por unidad de cada producto o servicio
+-- descuento_unitario: descuento aplicada a cada unidad de producto o servicio
+-- =============================================
+-- DE TABLA "ordenes_de_trabajo"
+-- monto_subtotal: suma de los precios unitarios de todos los productos y servicios
+-- descuento_productos_servicios: suma de los descuentos unitarios de todos los productos y servicios
+-- descuento_global: descuento aplicado a toda la orden de trabajo
+-- monto_total: monto final a pagar por la orden de trabajo.
+-- monto_neto: monto total sin incluir el IVA
+-- monto_iva: monto del IVA aplicado a la orden de trabajo
+-- =============================================
+
+DROP PROCEDURE IF EXISTS sp_calcular_total_orden_de_trabajo;
+
+CREATE PROCEDURE sp_calcular_total_orden_de_trabajo(
+    IN p_id_orden_de_trabajo INT,
+    OUT p_monto_total DECIMAL(12,0)
+)
+BEGIN
+    -- declaración de variables locales
+    DECLARE v_subtotal DECIMAL(12,0) DEFAULT 0;
+    DECLARE v_descuento_prod DECIMAL(12,0) DEFAULT 0;
+    DECLARE v_descuento_global DECIMAL(12,0) DEFAULT 0;
+    DECLARE v_total DECIMAL(12,0) DEFAULT 0;
+    DECLARE v_neto DECIMAL(12,0) DEFAULT 0;
+    DECLARE  v_iva DECIMAL(12,0) DEFAULT 0;
+
+    -- calcular subtotal bruto y total de descuentos
+    SELECT 
+        COALESCE(SUM(cantidad * precio_unitario), 0),
+        COALESCE(SUM(cantidad * descuento_unitario),0)
+    INTO 
+        v_subtotal,
+        v_descuento_prod
+    FROM lineas_de_orden_de_trabajo
+    WHERE id_orden_de_trabajo = p_id_orden_de_trabajo;
+
+    -- calcular descuento global
+    SELECT 
+        COALESCE(descuento_global, 0)
+    INTO v_descuento_global
+    FROM ordenes_de_trabajo
+    WHERE id_orden_de_trabajo = p_id_orden_de_trabajo;
+
+    -- calcular total y desglose iva
+    -- total = subtotal - descuentos
+    SET v_total = v_subtotal - v_descuento_prod - v_descuento_global;
+    SET v_neto = ROUND(v_total / 1.19, 0);
+    SET v_iva = v_total - v_neto;
+
+    -- actualizar valores
+    UPDATE ordenes_de_trabajo
+    SET 
+        monto_subtotal = v_subtotal,
+        descuento_productos_servicios = v_descuento_prod,
+        monto_neto = v_neto,
+        monto_iva = v_iva,
+        monto_total = v_total
+    WHERE id_orden_de_trabajo = p_id_orden_de_trabajo;
+
+    -- devolver el monto total calculado
+    SET p_monto_total = v_total;
+
+END;
+
+
