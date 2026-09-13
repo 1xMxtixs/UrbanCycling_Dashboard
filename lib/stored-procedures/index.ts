fbb208fd81a536540c51028dd653e7313f034c39
Amaro <amaroromeromoreno@gmail.com>
2026-09-13 00:44:42 -0300

    feat: ajustar cantidades de insumos

--- lib/stored-procedures/index.ts
+++ lib/stored-procedures/index.ts
@@ -4,9 +4,11 @@ export {
 } from "./work-order-totals";
 
 export {
+  ajustarStockPorCantidadLineaOrden,
   descontarStockProductos,
   registrarMovimientoConAjusteStock,
   InventoryStockError,
+  type AjusteStockLineaOrdenInput,
   type ProductoParaDescontar,
 } from "./inventory-stock";
 
